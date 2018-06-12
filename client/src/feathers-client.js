fb8ef157f7d487b5a0f4aa11839a5b6c639278c0
CJ R <cj@null.computer>
2018-06-11 22:58:00 -0600

    :rocket: Prepare for client deploy

--- client/src/feathers-client.js
+++ client/src/feathers-client.js
@@ -3,7 +3,13 @@ import socketio from '@feathersjs/socketio-client';
 import auth from '@feathersjs/authentication-client';
 import io from 'socket.io-client';
 
-const socket = io('http://localhost:3030', {
+let API_URL = 'https://cg-trello-clone.now.sh';
+
+if (window.location.hostname === 'localhost') {
+  API_URL = 'http://localhost:3030';
+}
+
+const socket = io(API_URL, {
   transports: ['websocket'],
 });
 
