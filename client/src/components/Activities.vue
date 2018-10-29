0416d20e6e7c184d887ed90caa605b77dbdfc005
vitalijalbu <40239902+vitalijalbu@users.noreply.github.com>
2018-10-29 10:08:22 +0100

    Update Activities.vue

--- client/src/components/Activities.vue
+++ client/src/components/Activities.vue
@@ -13,6 +13,7 @@
               <v-icon color="green">local_activity</v-icon>
             </v-list-tile-action>
             <v-list-tile-content>
+            <div class="></div>
               <!-- <v-list-tile-title v-text="activity.userId"></v-list-tile-title> -->
               <v-list-tile-sub-title v-html="markdownify(activity.text)"></v-list-tile-sub-title>
             </v-list-tile-content>
